5056834769c6048dbf95871e37e74c7c81014428
rytis <rytis.janceris@gmail.com>
2018-10-06 17:05:47 +0300

    Added text and switch in main activity

--- app/src/main/java/com/example/rytis/customnavigationbuttons/MainActivity.java
+++ app/src/main/java/com/example/rytis/customnavigationbuttons/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.rytis.customnavigationbuttons;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button setupButton = findViewById(R.id.button);
-        setupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ButtonSetupActivity.class));
-            }
-        });
+
 
     }
 }
